5360faa6b31322d3a5cce58abe87c6698273f9d5
Leonardo Hernández <leonardoahl1507@gmail.com>
2023-09-23 18:39:31 -0600

    feat: spring project

---
+++ pering-backend/src/main/java/org/perryCode/peringbackend/PeringBackendApplication.java
@@ -0,0 +1,13 @@
+package org.perryCode.peringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeringBackendApplication.class, args);
+	}
+
+}
---
+++ pering-backend/src/test/java/org/perryCode/peringbackend/PeringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perryCode.peringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
