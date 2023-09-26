32e6f323a2d8061e7b3e1a8c62b8b050250dcb6c
Leonardo Hernández <leonardoahl1507@gmail.com>
2023-09-25 23:45:12 -0600

    feat:added service to user

--- pering-backend/src/main/java/org/perryCode/peringbackend/controller/UserController.java
+++ pering-backend/src/main/java/org/perryCode/peringbackend/controller/UserController.java
@@ -1,11 +1,17 @@
 package org.perryCode.peringbackend.controller;
 
 
+import java.util.List;
+
 import org.perryCode.peringbackend.entity.User;
-import org.perryCode.peringbackend.repository.UserRepository;
+import org.perryCode.peringbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	@Autowired
-	UserRepository userRepository;
+	UserService userService;
 
 	@GetMapping("{id}")
-	public User getUserById(@PathVariable long id) {
-		User customer = userRepository.findById(id);
+	public ResponseEntity<User> getUserById(@PathVariable long id) {
+		User customer = userService.getUserById(id);
 		
-		return customer;	
+		return new ResponseEntity<User>(customer, HttpStatus.OK);	
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<User>> getAllUsers(){
+		List<User> users = userService.getAllUsers();
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User newUser = userService.createUser(user);
+		return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
 	}
 	
 	
