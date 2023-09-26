c56ff3e28fa1ed9c757a64a6bd317a967302762b
Jimena Barranco Medina <Jime@192.168.1.6>
2023-09-25 22:17:52 -0600

    feat: service and service impl -userhassoftskills

--- pering-backend/src/main/java/org/perryCode/peringbackend/controller/UserHasSoftSkillController.java
+++ pering-backend/src/main/java/org/perryCode/peringbackend/controller/UserHasSoftSkillController.java
@@ -1,13 +1,16 @@
 package org.perryCode.peringbackend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.perryCode.peringbackend.entity.UserHasSoftSkill;
-import org.perryCode.peringbackend.repository.UserHasSoftSkillRepository;
+import org.perryCode.peringbackend.service.UserHasSoftSkillService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,19 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserHasSoftSkillController {
 
 	  @Autowired
-	    private UserHasSoftSkillRepository userHasSoftSkillsRepository;
-
-	    // Endpoint para obtener todos los registros de users_has_soft_skills
-	    @GetMapping
-	    public List<UserHasSoftSkill> getAllUserSoftSkills() {
-	        return userHasSoftSkillsRepository.findAll();
+	    private UserHasSoftSkillService userHasSoftSkillsService;
+	  
+	  
+	  @GetMapping//Solicitud get para userHasSoftSkill (all)
+	    public ResponseEntity<List<UserHasSoftSkill>> getAllUserHasSoftSkill() {
+	    	List<UserHasSoftSkill> userHasSoftSkills = (List<UserHasSoftSkill>)userHasSoftSkillsService.getAllUserHasSoftSkill();
+	        return ResponseEntity.ok(userHasSoftSkills);
 	    }
-
-	    // Endpoint para crear un nuevo registro
-	    @PostMapping
-	    public UserHasSoftSkill createUserSoftSkills(@RequestBody UserHasSoftSkill userSoftSkills) {
-	        return userHasSoftSkillsRepository.save(userSoftSkills);
+	  
+	    
+	  
+	  @GetMapping("/{id}")//Solicitud get para userHasSoftSkill por id
+	    public ResponseEntity<UserHasSoftSkill> getSoftSkillById(@PathVariable long id) {
+	        Optional<UserHasSoftSkill> userHasSoftSkill = userHasSoftSkillsService.getUserHasSoftSkillById(id);
+	        return userHasSoftSkill.map(ResponseEntity::ok)
+	                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	    }
+	  
+	    
+	  @DeleteMapping("/{id}")//Solicitud delete para eliminar userHasSoftSkill por id
+	  public ResponseEntity<String> deleteSoftSkill(@PathVariable long id) {
+		  	userHasSoftSkillsService.deleteUserHasSoftSkills(id);
+	        return ResponseEntity.ok("SoftSkill deleted successfully");
+	    }
+	    
+	 
     
   
 }
