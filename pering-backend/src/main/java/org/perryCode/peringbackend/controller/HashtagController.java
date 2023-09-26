d83c2acf5187dd5bdb2369faa1f6b3bfbe4f1c75
Jimena Barranco Medina <Jime@192.168.1.6>
2023-09-26 12:16:53 -0600

    fix: http responses

--- pering-backend/src/main/java/org/perryCode/peringbackend/controller/HashtagController.java
+++ pering-backend/src/main/java/org/perryCode/peringbackend/controller/HashtagController.java
@@ -2,8 +2,6 @@ package org.perryCode.peringbackend.controller;
 import java.util.List;
 import java.util.Optional;
 import org.perryCode.peringbackend.entity.Hashtag;
-
-
 import org.perryCode.peringbackend.service.HashtagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,9 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,31 +24,24 @@ public class HashtagController {
     @GetMapping//Solicitud get para todos los hashtag
     public ResponseEntity<List<Hashtag>> getAllHashtags() {
         List<Hashtag> hashtags = (List<Hashtag>) hashtagService.getAllHashtags();
-        return ResponseEntity.ok(hashtags);
+        
+        return new ResponseEntity<List<Hashtag>>(hashtags, HttpStatus.OK);
+       
     }
 
     @GetMapping("/{id}")//Solicitud get para hashtag por id
     public ResponseEntity<Hashtag> getHashtagById(@PathVariable long id) {
+    	
         Optional<Hashtag> hashtag = hashtagService.getHashtagById(id);
-        return hashtag.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        
+        return new ResponseEntity<Hashtag>(hashtag.get(),HttpStatus.OK);
     }
 
-    @PostMapping//Solicitud post para hashtag
-    public ResponseEntity<Hashtag> setHashtag(@RequestBody Hashtag hashtag) {
-        Hashtag newHashtag = hashtagService.setHashtag(hashtag);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newHashtag);
-    }
-
-    @PutMapping("/{id}")//Solicitud put para actualizar hashtag por id
-    public ResponseEntity<Hashtag> updateHashtag(@RequestBody Hashtag hashtag, @PathVariable long id) {
-        Hashtag updatedHashtag = hashtagService.updateHashtag(hashtag, id);
-        return ResponseEntity.ok(updatedHashtag);
-    }
 
     @DeleteMapping("/{id}")//Solicitud delete para elimirar hashtag por id
     public ResponseEntity<String> deleteHashtag(@PathVariable long id) {
         hashtagService.deleteHashtag(id);
+        
         return ResponseEntity.ok("Hashtag deleted successfully");
     }
 
--- pering-backend/src/main/java/org/perryCode/peringbackend/controller/SoftSkillController.java
+++ pering-backend/src/main/java/org/perryCode/peringbackend/controller/SoftSkillController.java
@@ -7,13 +7,9 @@ import org.perryCode.peringbackend.service.SoftSkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,34 +23,24 @@ public class SoftSkillController {
     @GetMapping
     public ResponseEntity<List<SoftSkill>> getAllSoftSkills() {
     	List<SoftSkill> softSkills = (List<SoftSkill>)softSkillService.getAllSoftSkills();
-        return ResponseEntity.ok(softSkills);
+    	
+    	return new ResponseEntity<List<SoftSkill>>(softSkills, HttpStatus.OK);
+        
     }
     
     @GetMapping("/{id}")//Solicitud get para soft skill por id
     public ResponseEntity<SoftSkill> getSoftSkillById(@PathVariable long id) {
         Optional<SoftSkill> softSkill = softSkillService.getSoftSkillById(id);
-        return softSkill.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        
+        return new ResponseEntity<SoftSkill>(softSkill.get(),HttpStatus.OK);
     }
     
 
-    @PostMapping//Solicitud post para soft skill
-    public ResponseEntity<SoftSkill> setSoftSkill(@RequestBody SoftSkill SoftSkill) {
-        SoftSkill newSoftSkill = softSkillService.setSoftSkill(SoftSkill);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newSoftSkill);
-    }
-    
-
-    @PutMapping("/{id}")//Solicitud put para actualizar soft skill por id
-    public ResponseEntity<SoftSkill> updateSoftSkill(@RequestBody SoftSkill SoftSkill, @PathVariable long id) {
-        SoftSkill updatedSoftSkill = softSkillService.updateSoftSkill(SoftSkill, id);
-        return ResponseEntity.ok(updatedSoftSkill);
-    }
-    
 
     @DeleteMapping("/{id}")//Solicitud delete para elimirar soft skill por id
     public ResponseEntity<String> deleteSoftSkill(@PathVariable long id) {
     	softSkillService.deleteSoftSkill(id);
+    	
         return ResponseEntity.ok("SoftSkill deleted successfully");
     }
     
