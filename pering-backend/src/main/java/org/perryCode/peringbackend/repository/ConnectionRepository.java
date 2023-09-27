e9ec0ae3ca9da8c1963783b039d5b28a5a985e03
Leonardo Hernández <leonardoahl1507@gmail.com>
2023-09-26 23:23:35 -0600

    feat: added connection

---
+++ pering-backend/src/main/java/org/perryCode/peringbackend/repository/ConnectionRepository.java
@@ -0,0 +1,8 @@
+package org.perryCode.peringbackend.repository;
+
+import org.perryCode.peringbackend.entity.Connection;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConnectionRepository extends CrudRepository<Connection, Long> {
+
+}
