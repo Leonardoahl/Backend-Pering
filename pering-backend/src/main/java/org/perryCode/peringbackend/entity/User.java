dc4461440696c2cbede26b2c60849c22f2bc318b
Leonardo Hernández <leonardoahl1507@gmail.com>
2023-09-28 22:46:03 -0600

    tech skills many to many

--- pering-backend/src/main/java/org/perryCode/peringbackend/entity/User.java
+++ pering-backend/src/main/java/org/perryCode/peringbackend/entity/User.java
@@ -8,10 +8,15 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -43,5 +48,15 @@ public class User{
 	@OneToMany(mappedBy = "user")
 	@JsonIgnoreProperties("user")
 	private List<Post> posts = new ArrayList<>();
+	
+	
+	
+	@ManyToMany(fetch= FetchType.EAGER, cascade = CascadeType.MERGE)
+	@JoinTable(
+			name = "users_has_tech_skills", joinColumns = @JoinColumn(name = "fk_users_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "fk_tech_skills_id", referencedColumnName = "id")
+			)
+	private List<TechSkill> techSkills = new ArrayList<>();
+	
 
 }
